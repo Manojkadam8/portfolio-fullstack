13a16a4a2f84584a82333953f8819a8453d5a0bb
Manojkadam8 <kadammanoj7200@gmail.com>
2026-03-26 20:58:16 +0530

    fix: contact model path

--- Backend/src/models/contact.js
+++ Backend/src/models/contact.js
@@ -7,4 +7,4 @@ const contactSchema = new mongoose.Schema({
   message: String
 }, { timestamps: true });
 
-export default mongoose.model("Contact", contactSchema);
+export default mongoose.model("contact", contactSchema);
